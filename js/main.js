5eba4b2f7179dcbf7e08c4d896c75a829f116b9f
BurkR <46469571+BurkR@users.noreply.github.com>
2019-02-04 08:40:26 -0800

    updated w/ legend code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+/ for legend 
+var labels = [];
+for (var i = 0; i < grades.length - 1; i++) {
+    labels.push('<i style="background:' + colors[i] + '"></i> ' + grades[i] + ' - ' + grades[i + 1]);
+}
+labels.push('<i style="background:' + colors[grades.length - 1] + '"></i> ' + grades[grades.length - 1] + ' +');
+$(".legend").html(labels.join('<br>'));
